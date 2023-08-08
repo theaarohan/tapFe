22c778c45fb9518398c390204878899b2c431840
scriptscrypt <tirumaladasa@gmail.com>
2023-08-08 14:08:07 +0530

    Added Vercel.json

--- src/apiInteraction/apiCalls.js
+++ src/apiInteraction/apiCalls.js
@@ -1,8 +1,12 @@
+const APIURL = "http://localhost:3000";
+
+
+
+
 import axios from 'axios';
-const APIURL = "http://localhost:3000"
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Replace with your backend URL
+  baseURL: APIURL, // Replace with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
